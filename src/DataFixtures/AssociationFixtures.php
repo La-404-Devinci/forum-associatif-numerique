2e29bcbd26a6debd03bd129f6e8ed34e281eb50f
Nicolas de Garrigues <ndegarrigues@gmail.com>
2021-07-16 16:40:32 +0200

    login form done

--- src/DataFixtures/AssociationFixtures.php
+++ src/DataFixtures/AssociationFixtures.php
@@ -5,15 +5,24 @@ namespace App\DataFixtures;
 use App\Entity\Association;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AssociationFixtures extends Fixture
 {
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $association = new Association();
 
+        $password = $this->encoder->encodePassword($association, 'password');
+
         $association->setName('La 404 DeVinci');
-        $association->setPassword('password');
+        $association->setPassword($password);
         $association->setLogoColor('aled');
         $association->setBanner('aled');
         $association->setStatus('/');
