3cf3f504b318a5834778327f3bb7908846f7aa0f
ProfCitron <jeveuxparler@proficitron.fr>
2021-07-18 11:06:43 +0200

    :clown_face: Mockup : Header banner component

--- assets/app.js
+++ assets/app.js
@@ -14,6 +14,7 @@ import AOS from "aos";
 import "@fortawesome/fontawesome-free";
 AOS.init();
 
+
 document.querySelector('#navigation-burger').addEventListener('click', function(){
     this.classList.toggle('active')
     document.querySelector('.navigation__main-menu-content').classList.toggle('active')
