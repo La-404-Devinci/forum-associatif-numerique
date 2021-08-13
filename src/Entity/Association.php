144122e6e3514acbd6abf91f130ffe7eb7c2890b
Nicolas de Garrigues <ndegarrigues@gmail.com>
2021-08-13 18:01:50 +0200

    update slug

--- src/Entity/Association.php
+++ src/Entity/Association.php
@@ -288,7 +288,11 @@ class Association implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function setSlug(): self
     {
-        $this->slug = strtolower(str_replace('&','and',str_replace("'",'',str_replace(' ', '-', $this->name))));
+
+        
+        $search  = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ');
+        $replace = array('A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y');
+        $this->slug = str_replace($search, $replace, strtolower(str_replace('&','and',str_replace("'",'',str_replace(' ', '-', $this->name)))));
 
         return $this;
     }
